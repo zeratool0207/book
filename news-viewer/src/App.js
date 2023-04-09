804d951a3926021af258109b5eeafa2a8573337c
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-09 18:49:21 +0900

    news viewer UI 생성

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,9 @@
 import React,{useState} from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [ data, setData ] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=fbdf37320f5b44169d051b411419cd4b',
-      );
-      setData(response.data)
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly = {true} /> }
-    </div>
+    <NewsList />
   );
 }
 
